77c55a7b11f0816c2677535157625abb16e11289
Dante <dante.gs92@gmail.com>
2017-06-19 16:32:20 -0300

    Delete production.js

--- config/production.js
+++
@@ -1,9 +0,0 @@
-var config = module.exports = {};
-
-config.env = 'production';
-config.hostname = 'copaviejogasometro.com';
-
-//mongo database
-config.mongo = {};
-config.mongo.uri = process.env.MONGO_URI || 'localhost';
-config.mongo.db = 'casla';
