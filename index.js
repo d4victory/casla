3e9099ba2af47802ca09b49f51306d11928d2c6f
Dante <dante.gs92@gmail.com>
2017-06-19 17:35:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var express         = require("express"),
     cookieParser    = require('cookie-parser'),
     session         = require('express-session'),
     bodyParser      = require('body-parser'),
-    config          = require('./config'),
+    config          = require('config'),
     port            = process.env.PORT || 8080,
     options         = require("options"),
     Client          = require('node-rest-client').Client;
@@ -21,11 +21,6 @@ var logger = require('./logger');
 // Connection to DB
 
 //db connection
-var env = process.env.NODE_ENV || 'development'
-  , cfg = require('./config.'+env);
-
-module.exports = cfg;
-
 mongoose.connect(cfg.mongo.uri, function(err, res) {
   if(err) throw err;
   console.log('Connected to Database');
