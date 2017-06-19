ae45c04f7ca0a0753aa08b251dee2f38578aa342
Dante <dante.gs92@gmail.com>
2017-06-19 16:55:43 -0300

    Update config.global.js

--- config/config.global.js
+++ config/config.global.js
@@ -1,8 +1,10 @@
 var config = module.exports = {};
 
 config.env = 'development';
+config.hostname = 'localhost';
+config.mongo.db = 'casla_dev';
 
 //mongo database
 config.mongo = {};
 config.mongo.uri = process.env.MONGO_URI || 'localhost';
-config.mongo.db = 'mongodb://localhost:3000/casla_dev';
+config.mongo.db = 'mongodb://'+config.hostname+':3000/'+config.mongo.db+';
