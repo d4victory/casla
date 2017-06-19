2d8a4bbb077daea0ada2d74a161af5bfbde35067
Dante <dante.gs92@gmail.com>
2017-06-19 14:48:32 -0300

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -5,10 +5,6 @@
  */
 
 module.exports = {
-  db: 'mongodb://localhost:3000/casla',
-  github: {
-    clientID: process.env.GITHUB_CLIENTID,
-    clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
-  }
+  // db url
+  db: "mongodb://localhost:3000/casla_dev"
 };
