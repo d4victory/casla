2de5dc48f61df5c88a0fd646637f4ac779568cdf
Dante <dante.gs92@gmail.com>
2017-06-19 16:36:29 -0300

    Create config.production.js

---
+++ config/config.production.js
@@ -0,0 +1,6 @@
+var config = require('./config.global');
+
+config.env = 'production';
+config.mongo.db = 'mongodb://copaviejogasometro:Ka1438657@ds123182.mlab.com:23182/casla';
+
+module.exports = config;
