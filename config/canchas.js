edb0a52c9401ff11ba215befd0b5b86336a68a69
Dante <dante.gs92@gmail.com>
2017-06-21 14:18:54 -0300

    Update canchas.js

--- config/canchas.js
+++ config/canchas.js
@@ -1,8 +1,10 @@
 module.exports = function(app,isAdmin) {
+    
+    var cfg = require('../config');
 
     app.post('/nuevaCancha', isAdmin, function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
-        client.get("http://localhost:3000/torneo/"+req.body.torneoid, function (torneo, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/torneo/""+req.body.torneoid, function (torneo, response) {
             res.render('./ejs/canchas/agregarCancha.ejs', {user: req.user, torneo: torneo, divisiones:divisiones, message: req.flash('loginMessage')});
         });
         });
@@ -14,16 +16,16 @@ module.exports = function(app,isAdmin) {
             data:  req.body ,
             headers: { "Content-Type": "application/json" }
         };
-        client.post("http://localhost:3000/cancha", args, function (data, response) {
+        client.post("http://"+cfg.hostname+"/cancha", args, function (data, response) {
             res.redirect('/canchasDelTorneo?torneoid='+data.torneo._id);
         });
     });
 
     app.get('/canchasDelTorneo', isAdmin, function(req, res) {
         console.log('estoy en /canchasDelTorneo');
-        client.get("http://localhost:3000/torneo/"+req.query.torneoid, function (torneo, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
-                client.get("http://localhost:3000/cancha/torneo/"+req.query.torneoid, function (canchas, response) {
+        client.get("http://"+cfg.hostname+"/torneo/"+req.query.torneoid, function (torneo, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+                client.get("http://"+cfg.hostname+"/cancha/torneo/"+req.query.torneoid, function (canchas, response) {
                     res.render('./ejs/canchas/canchasDelTorneo.ejs', {user: req.user, canchas: canchas, divisiones:divisiones,
                         torneo:torneo,  message: req.flash('loginMessage')});
                 });
