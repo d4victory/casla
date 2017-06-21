a776713f410666044af68c173809e8fe9a67cf52
Dante <dante.gs92@gmail.com>
2017-06-20 22:10:37 -0300

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(express,app, passport, client, logger) {
+module.exports = function(express,app, passport, client, logger, args) {
 
     require('../RESTServices/jugadorRESTService')(express,app);
     require('../RESTServices/torneoRESTService')(express,app);
@@ -17,7 +17,7 @@ module.exports = function(express,app, passport, client, logger) {
     app.get('/', function(req, res) {
 	console.log('estoy en home page');
 	
-        client.get(cfg.mongo.uri+"/division", function (divisiones, response) {
+        client.get(cfg.mongo.uri+"/division",args, function (divisiones, response) {
             res.render('./ejs/index.ejs', {user: req.user, divisiones:divisiones})
         });
 	res.redirect('/');
