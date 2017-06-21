3a811f50e3a662d23a74f3e939aac7f3965e72f1
Dante <dante.gs92@gmail.com>
2017-06-21 14:29:20 -0300

    Update planilleros.js

--- config/planilleros.js
+++ config/planilleros.js
@@ -1,19 +1,22 @@
 //var moment = require('moment');
 
 module.exports = function(app) {
+    
+    var cfg = require('../config');
+    
     app.get('/planillero', isPlanillero, function(req, res) {
-        client.get("http://localhost:3000/partido/estado/N.E", function (partidos, response) {
-            client.get("http://localhost:3000/equipo", function (equipos, response) {
+        client.get("http://"+cfg.hostname+"/partido/estado/N.E", function (partidos, response) {
+            client.get("http://"+cfg.hostname+"/equipo", function (equipos, response) {
                 var equiposMap =  {};
                 for (var i = 0; i < equipos.length; i++) {
                     equiposMap[equipos[i]._id] = equipos[i].nombre;
                 };
-                client.get("http://localhost:3000/division", function (divisiones, response) {
+                client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
                     var divisionesMap =  {};
                     for (var i = 0; i < divisiones.length; i++) {
                         divisionesMap[divisiones[i]._id] = divisiones[i].nombre;
                     };
-                    client.get("http://localhost:3000/partido/numeros_fechas", function (numeros_fechas, response) {
+                    client.get("http://"+cfg.hostname+"/partido/numeros_fechas", function (numeros_fechas, response) {
                         //res.render('./ejs/planilleros/planillero.ejs', {user: req.user, partidos:partidos, message: req.flash('loginMessage'),
                         //										numeros_fechas:numeros_fechas, divisiones:divisionesMap, equipos:equiposMap, resultado: req.session.statusDelete});
                         res.render('./ejs/partidos/partidos.ejs', {user: req.user,
@@ -32,8 +35,8 @@ module.exports = function(app) {
 
     app.get('/cargarPartido', isPlanillero, function(req, res) {
         console.log('estoy en GET de /cargarPartido, partidoid='+req.query.partidoid);
-        client.get("http://localhost:3000/partido/"+req.query.partidoid, function (partido, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/partido/"+req.query.partidoid, function (partido, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
                 res.render('./ejs/partidos/cargarPartido.ejs', {user: req.user, divisiones:divisiones, partido: partido, message: req.flash('loginMessage'), resultado: req.session.statusSaved});
             });
         });
@@ -49,7 +52,7 @@ module.exports = function(app) {
 
         console.log(req.body);
 
-        client.put("http://localhost:3000/partido/"+req.query.partidoid, args, function (data, response) {
+        client.put("http://"+cfg.hostname+"/partido/"+req.query.partidoid, args, function (data, response) {
             data.data["equipo1Old"] = data.equipo1Old;
             data.data["equipo2Old"] = data.equipo2Old;
             data.data["status"] = data.status;
@@ -58,7 +61,7 @@ module.exports = function(app) {
                 data:  data.data ,
                 headers: { "Content-Type": "application/json" }
             };
-            client.post("http://localhost:3000/posicionEquipo/updatePosicionEquipo/", args2, function (data, response) {
+            client.post("http://"+cfg.hostname+"/posicionEquipo/updatePosicionEquipo/", args2, function (data, response) {
                 console.log("PUT /partido");
                 res.redirect('/partidos');
             });
