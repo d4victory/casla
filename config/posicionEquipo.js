d0f5a7f2a43a32f6365e731e0ee705be29a695b6
Dante <dante.gs92@gmail.com>
2017-06-21 14:30:10 -0300

    Update posicionEquipo.js

--- config/posicionEquipo.js
+++ config/posicionEquipo.js
@@ -1,8 +1,11 @@
 module.exports = function(app) {
+    
+    var cfg = require('../config');
+    
     app.get('/posicionesDeLaDivision/', function(req, res) {
-        client.get("http://localhost:3000/posicionEquipo/division/"+req.query.divisionid, function (posicionEquipo, response) {
-            client.get("http://localhost:3000/division/", function (divisiones, response) {
-                client.get("http://localhost:3000/division/"+req.query.divisionid, function (division, response) {
+        client.get("http://"+cfg.hostname+"/posicionEquipo/division/"+req.query.divisionid, function (posicionEquipo, response) {
+            client.get("http://"+cfg.hostname+"/division/", function (divisiones, response) {
+                client.get("http://"+cfg.hostname+"/division/"+req.query.divisionid, function (division, response) {
                     res.render('./ejs/divisiones/posicionesDeLaDivision.ejs', {user: req.user, posicionEquipo:posicionEquipo, divisiones: divisiones, division:division,message: req.flash('loginMessage')});
                 });
             });
