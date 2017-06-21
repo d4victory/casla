f6e2a6c21c8fff12e4f9322012a67d547b730b75
Dante <dante.gs92@gmail.com>
2017-06-21 11:43:53 -0300

    Update config.production.js

--- config/config.production.js
+++ config/config.production.js
@@ -1,8 +1,8 @@
 var config = require('./config.global');
 
 config.env = 'production';
-config.hostname = 'mongodb://copaviejogasometro:Ka1438657@ds115752.mlab.com:15752';
-config.mongo.db = 'heroku_15ct5z8j';
+config.hostname = 'mongodb://copaviejogasometro:Ka1438657@ds123182.mlab.com:23182';
+config.mongo.db = 'casla';
 config.mongo.uri = config.hostname+'/'+config.mongo.db;
 
 module.exports = config;
