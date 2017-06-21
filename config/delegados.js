35626e96958d1b541255932210c3023422e6609f
Dante <dante.gs92@gmail.com>
2017-06-21 13:15:36 -0300

    Update delegados.js

--- config/delegados.js
+++ config/delegados.js
@@ -1,11 +1,13 @@
 var moment = require('moment');
 
 module.exports = function(app) {
+    var cfg = require('../config');
+
     app.get('/delegado', isDelegado, function(req, res) {
         if (req.user.equipo){
-            client.get("http://localhost:3000/equipo/"+req.user.equipo, function (equipo, response) {
-                client.get("http://localhost:3000/division", function (divisiones, response) {
-                    client.get("http://localhost:3000/jugador/equipo/"+req.user.equipo, function (jugadores, response) {
+            client.get("http://"+cfg.hostname+"/equipo"+req.user.equipo, function (equipo, response) {
+                client.get"http://"+cfg.hostname+"/division", function (divisiones, response) {
+                    client.get("http://"+cfg.hostname+"/jugador/equipo/"+req.user.equipo, function (jugadores, response) {
                         res.render('./ejs/delegados/miEquipo.ejs', {user: req.user, equipo:equipo, message: req.flash('loginMessage'),
                             jugadores:jugadores, divisiones:divisiones, resultado: req.session.statusDelete});
                     });
@@ -18,8 +20,8 @@ module.exports = function(app) {
     });
 
     app.get('/addJugador', isDelegado, function(req, res) {
-        client.get("http://localhost:3000/equipo/"+req.user.equipo, function (equipo, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/equipo"+req.user.equipo, function (equipo, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
                 res.render('./ejs/delegados/agregarJugador.ejs', {user: req.user, divisiones:divisiones, equipo:equipo, message: req.flash('loginMessage'),
                     resultado: req.session.statusDelete});
             });
@@ -27,12 +29,11 @@ module.exports = function(app) {
     });
 
     app.post('/datosJugador', isDelegado, function(req, res) {
-        client.get("http://localhost:3000/jugador/"+req.body.jugadorid, function (jugador, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
-                client.get("http://localhost:3000/equipo/"+req.user.equipo, function (equipo, response) {
-                    res.render('./ejs/delegados/datosJugador.ejs', {user: req.user, equipo:equipo, jugador:jugador, message: req.flash('loginMessage'),
-                        resultado: req.session.statusDelete, divisiones:divisiones, moment:moment});
-                });
+        client.get("http://"+cfg.hostname+"/equipo/"+req.body.jugadorid, function (jugador, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+            client.get("http://"+cfg.hostname+"/equipo"+req.user.equipo, function (equipo, response) {
+                res.render('./ejs/delegados/datosJugador.ejs', {user: req.user, equipo:equipo, jugador:jugador, message: req.flash('loginMessage'),
+                    resultado: req.session.statusDelete, divisiones:divisiones, moment:moment});
             });
         });
 
@@ -59,14 +60,14 @@ module.exports = function(app) {
             data:  req.body ,
             headers: { "Content-Type": "application/json" }
         };
-        client.post("http://localhost:3000/jugador", args, function (data, response) {
+        client.post("http://"+cfg.hostname+"/jugador", args, function (data, response) {
             console.log("POST /jugador");
             res.redirect('/delegado');
         });
     });
 
     app.post('/deleteJugador', isDelegado, function(req, res) {
-        client.delete("http://localhost:3000/jugador/"+req.body.jugadorid, function (data, response) {
+        client.delete("http://"+cfg.hostname+"/jugador/"+req.body.jugadorid, function (data, response) {
             console.log("DELETE /jugador/"+req.body.jugadorid);
             req.session.statusDelete = response.statusCode;
             res.redirect('/delegado');
@@ -79,7 +80,7 @@ module.exports = function(app) {
 // route middleware to make sure a user is logged in (DELEGADO)
 function isDelegado(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
+    // if user is authenticated in the session, carry on
     if ((req.isAuthenticated()) && ( (req.user.role == "DELEGADO") || (req.user.role == "SUPER_ADMIN")))
         return next();
 
