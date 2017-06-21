724317f367480821eafa49d6b2f7b4d75ec2ad3a
Dante <dante.gs92@gmail.com>
2017-06-21 11:09:10 -0300

    Update config.production.js

--- config/config.production.js
+++ config/config.production.js
@@ -1,9 +1,9 @@
 var config = require('./config.global');
 
 config.env = 'production';
-//config.hostname = 'copaviejogasometro';
-//config.mongo.db = 'mongodb://'+config.hostname+':Ka1438657@ds123182.mlab.com:23182/casla';
-config.mongo.uri = 'mongodb://copaviejogasometro:Ka1438657@ds123182.mlab.com:23182/casla';
+config.hostname = 'mongodb://copaviejogasometro:Ka1438657@ds123182.mlab.com:23182';
+config.mongo.db = 'casla';
+config.mongo.uri = config.hostname+'/'+config.mongo.db;
 
 module.exports = config;
 
