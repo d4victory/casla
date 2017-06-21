3299e12966cf48d4ceefc5752d341eb95164be09
Dante <dante.gs92@gmail.com>
2017-06-21 13:09:46 -0300

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -14,12 +14,9 @@ module.exports = function(express,app, passport, client, logger) {
 	// =====================================
     // HOME PAGE (with login links) ========
     // =====================================
-    app.get('/', function(req, res) {
-	console.log('estoy en home page');
-	console.log('le voy a pegar a:'+cfg.hostname+'/division');
-	
+    app.get('/', function(req, res) {	
 	    //cfg.hostname
-        client.get("http://ds123182.mlab.com:23182/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
             res.render('./ejs/index.ejs', {user: req.user, divisiones:divisiones})
         });
     });
@@ -36,50 +33,48 @@ module.exports = function(express,app, passport, client, logger) {
     // LOGIN ===============================
     // =====================================
     // show the login form
-    //app.get('/login', function(req, res) {
-      //  client.get("http://localhost:3000/division", function (divisiones, response) {
-       //     res.render('./ejs/usuarios/login.ejs', {user: req.user, divisiones:divisiones, message: req.flash('loginMessage')})
+    app.get('/login', function(req, res) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+            res.render('./ejs/usuarios/login.ejs', {user: req.user, divisiones:divisiones, message: req.flash('loginMessage')})
+        });
 
-       // });
-
-   // });
+    });
 
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-   // app.get('/profile', isLoggedIn, function(req, res) {
-      //  client.get("http://localhost:3000/division", function (divisiones, response) {
-        //    res.render('./ejs/login/profile.ejs', {user: req.user, divisiones:divisiones})
-
-       // });
-  //  });
+    app.get('/profile', isLoggedIn, function(req, res) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+            res.render('./ejs/login/profile.ejs', {user: req.user, divisiones:divisiones})
+        });
+    });
 
     // =====================================
     // LOGOUT ==============================
     // =====================================
-   // app.get('/logout', function(req, res) {
-     //   req.session.destroy(function (err) {
-        //    res.clearCookie('connect.sid');
-        //   res.redirect('/'); 
-      //  });
-  //  });
+    app.get('/logout', function(req, res) {
+        req.session.destroy(function (err) {
+            res.clearCookie('connect.sid');
+           res.redirect('/'); 
+        });
+    });
 
 
     // process the signup form
-  //  app.post('/signup', passport.authenticate('local-signup', {
-    //    successRedirect : '/usuarios', // redirect to the secure profile section
-     //   failureRedirect : '/usuarios', // redirect back to the signup page if there is an error
-     //   failureFlash : true // allow flash messages
-  //  }));
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/usuarios', // redirect to the secure profile section
+        failureRedirect : '/usuarios', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-  //  app.post('/signupadmin', passport.authenticate('local-signup-admin', {
-   //     successRedirect : '/usuarios', // redirect to the secure profile section
-     //   failureRedirect : '/usuarios', // redirect back to the signup page if there is an error
-      //  failureFlash : true // allow flash messages
-   // }));
-/*
+    app.post('/signupadmin', passport.authenticate('local-signup-admin', {
+        successRedirect : '/usuarios', // redirect to the secure profile section
+        failureRedirect : '/usuarios', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
     app.post('/signupplanillero', passport.authenticate('local-signup-planillero', {
         successRedirect : '/usuarios', // redirect to the secure profile section
         failureRedirect : '/usuarios', // redirect back to the signup page if there is an error
@@ -95,35 +90,35 @@ module.exports = function(express,app, passport, client, logger) {
 
     //VIEWS
     app.get('/goleadores', function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
             res.render('./ejs/goleadores.ejs', {user: req.user, divisiones:divisiones})
         });
     });
 
     app.get('/sanciones', function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
             res.render('./ejs/sanciones.ejs', {user: req.user, divisiones:divisiones})
         });
     });
 
     app.get('/fairplay', function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
             res.render('./ejs/fairplay.ejs', {user: req.user, divisiones:divisiones})
         });
     });
 
     app.get('/superadmin', isSuperAdmin, function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
             res.render('./ejs/superadmin.ejs', {user: req.user, divisiones:divisiones})
         });
     });
 
-    // app.get('/posicionesDeLaDivision', function(req, res) {
-    //     client.get("http://localhost:3000/division/"+req.query.divisionId, function (division, response) {
-    //         res.render('./ejs/divisiones/posicionesDeLaDivision.ejs', {user: req.user, division: division, message: req.flash('loginMessage')});
-    //     });
-    //
-    // });
+     app.get('/posicionesDeLaDivision', function(req, res) {
+         client.get("http://"+cfg.hostname+"/division"+req.query.divisionId, function (division, response) {
+             res.render('./ejs/divisiones/posicionesDeLaDivision.ejs', {user: req.user, division: division, message: req.flash('loginMessage')});
+         });
+    
+     });
     
 
 }
@@ -166,4 +161,3 @@ function isSuperAdmin(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
-*/
