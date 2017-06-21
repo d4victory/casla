73475f6671bef671d91551bacad8c8b6650f7789
Dante <dante.gs92@gmail.com>
2017-06-21 14:26:57 -0300

    Update partidos.js

--- config/partidos.js
+++ config/partidos.js
@@ -1,9 +1,12 @@
 // var moment = require('moment');
 
 module.exports = function(app,isAdmin) {
+    
+    var cfg = require('../config');
+    
     app.get('/partidos', isAdmin, function(req, res) {
-        client.get("http://localhost:3000/division", function (divisiones, response) {
-            client.get("http://localhost:3000/partido/numeros_fechas", function (numeros_fechas, response) {
+        client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
+            client.get("http://"+cfg.hostname+"/partido/numeros_fechas", function (numeros_fechas, response) {
                 res.render('./ejs/partidos/partidos.ejs', { message: req.flash('signupMessage'), numeros_fechas:numeros_fechas,
                     user: req.user,divisiones:divisiones
                     ,resultado: req.session.statusDelete});
@@ -13,16 +16,16 @@ module.exports = function(app,isAdmin) {
 
     app.get('/partidosDelTorneo', isAdmin, function(req, res) {
         console.log('estoy en /partidosDelTorneo');
-        client.get("http://localhost:3000/torneo/"+req.query.torneoid, function (torneo, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/torneo/"+req.query.torneoid, function (torneo, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
                 res.render('./ejs/torneos/partidosTorneo.ejs', {user: req.user, divisiones:divisiones, torneo: torneo, message: req.flash('loginMessage')});
             });
         });
     });
 
     app.get('/agregarPartidos', isAdmin, function(req, res) {
-        client.get("http://localhost:3000/torneo", function (data, response) {
-            client.get("http://localhost:3000/division", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/torneo", function (data, response) {
+            client.get("http://"+cfg.hostname+"/division", function (divisiones, response) {
                 res.render('./ejs/partidos/agregarPartidos.ejs', {user: req.user, divisiones:divisiones, torneos: data, message: req.flash('loginMessage')});
             });
         });
@@ -33,14 +36,14 @@ module.exports = function(app,isAdmin) {
             data:  req.body ,
             headers: { "Content-Type": "application/json" }
         };
-        client.post("http://localhost:3000/partido", args, function (data, response) {
+        client.post("http://"+cfg.hostname+"/partido", args, function (data, response) {
             console.log("POST /partidos");
             res.redirect('/partidos');
         });
     });
 
     app.post('/deletePartido', isAdmin, function(req, res) {
-        client.delete("http://localhost:3000/partido/"+req.body.partidoid, function (data, response) {
+        client.delete("http://"+cfg.hostname+"/partido/"+req.body.partidoid, function (data, response) {
             data.data["equipo1Old"] = data.equipo1Old;
             data.data["equipo2Old"] = data.equipo2Old;
             data.data["statusOld"] = data.statusOld;
@@ -48,7 +51,7 @@ module.exports = function(app,isAdmin) {
                 data:  data.data ,
                 headers: { "Content-Type": "application/json" }
             };
-            client.post("http://localhost:3000/posicionEquipo/updatePosicionEquipo/", args2, function (data, response) {
+            client.post("http://"+cfg.hostname+"/posicionEquipo/updatePosicionEquipo/", args2, function (data, response) {
                 console.log("DELETE /partido/"+req.body.partidoid);
                 req.session.statusDelete = response.statusCode;
                 res.redirect('/partidos');
