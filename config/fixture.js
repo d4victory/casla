af43e2c570146e1d43fb3a8821dd6161109763aa
Dante <dante.gs92@gmail.com>
2017-06-21 14:24:57 -0300

    Update fixture.js

--- config/fixture.js
+++ config/fixture.js
@@ -2,9 +2,12 @@
  * Created by franc on 09/06/2017.
  */
 module.exports = function(app) {
+    
+    var cfg = require('../config');
+    
     app.get('/fixture/', function(req, res) {
-        client.get("http://localhost:3000/fixture/division/"+req.query.divisionid, function (partidos, response) {
-            client.get("http://localhost:3000/division/", function (divisiones, response) {
+        client.get("http://"+cfg.hostname+"/fixture/division/"+req.query.divisionid, function (partidos, response) {
+            client.get("http://"+cfg.hostname+"/division/", function (divisiones, response) {
                 res.render('./ejs/fixture/fixture.ejs', {user: req.user, divisiones: divisiones,partidos:partidos, message: req.flash('loginMessage')});
             });
         });
