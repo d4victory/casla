b4b6689def0e733f2ec65204e49675080f4d3bbe
Manuel Gambino <manuelgambinob@gmail.com>
2017-08-24 19:13:06 -0300

    sera esto

--- config/environments/development.js
+++ config/environments/development.js
@@ -2,7 +2,7 @@ module.exports = {
   db: 'mongodb://localhost:27017/casla_dev',
   hostname: 'localhost',
   port: 8080,
-  nodeClientUrl: 'http://localhost',
+  nodeClientUrl: 'http://localhost:8080',
   mongo: {
     db: 'casla_dev',
     uri: 'mongodb://localhost:27017/casla_dev',
