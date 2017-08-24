bbfe00efc9711137e78a5299112de9fe247b8b1b
Manuel Gambino <manuelgambinob@gmail.com>
2017-08-24 19:18:26 -0300

    mmm

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,7 +3,7 @@ module.exports = {
   env: 'production',
   hostname: 'keroku-casla.herokuapp.com',
   port: process.env.PORT, // heroku settings
-  nodeClientUrl: 'http://keroku-casla.herokuapp.com:' + process.env.PORT,
+  nodeClientUrl: 'http://localhost:' + process.env.PORT,
   mongo: {
     db: 'casla',
     uri: 'mongodb://copaviejogasometro:Ka1438657@ds123182.mlab.com:23182/casla',
