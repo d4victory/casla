f1d160026c834c663d1ee5cec480b09466bbaffe
Dante <dante.gs92@gmail.com>
2018-07-21 16:31:32 -0300

    fyi

--- config/environments/production.js
+++ config/environments/production.js
@@ -4,7 +4,7 @@ module.exports = {
   requestBase: 'http://localhost:' + process.env.PORT,
   mongo: {
     db: 'adnfutbol',
-    uri: 'mongodb://adnfutboluser:Ka1438657@ds021346.mlab.com:21346/adnfutbol',
+    uri: 'mongodb://adnfutboluser:Ka1438657@ds145881.mlab.com:45881/adnfutbol',
     username: 'adnfutboluser',
     password: 'Ka1438657'
   }
